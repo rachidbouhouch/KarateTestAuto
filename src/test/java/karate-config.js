c62cdfd0c7fb23ba8f4d9164ba407618be907ef8
Rachid bouhouch <rbouhouch@sqli.com>
2023-08-25 14:45:29 +0100

    create a random email function + random name function

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,17 +1,36 @@
-function fn() {    
+function fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
     env = 'dev';
   }
+  var randomEmail = function() {
+    var chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+    var email = "";
+    for (var i = 0; i < 10; i++) {
+      var randomIndex = Math.floor(Math.random() * chars.length);
+      email += chars.charAt(randomIndex);
+    }
+    return email + "@example.com";
+  };
+  var randomName = function() {
+    var firstNames = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+    var lastNames = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis"];
+
+    var randomFirstNameIndex = Math.floor(Math.random() * firstNames.length);
+    var randomLastNameIndex = Math.floor(Math.random() * lastNames.length);
+
+    return firstNames[randomFirstNameIndex] + " " + lastNames[randomLastNameIndex];
+  };
+
   var config = {
     env: env,
     myVarName: 'someValue',
     baseURL:'https://simple-books-api.glitch.me/',
-    clientName:'rachid',
-    clientEmail:'rachidbouhouch10000@gmail.net',
-
+    clientName: randomName(),
+    clientEmail: randomEmail()
   }
+
   if (env == 'dev') {
     // customize
     // e.g. config.foo = 'bar';
